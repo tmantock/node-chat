fe0bb4f8975baf717e1bd158162a03c7a06ff782
Tevin Mantock <tmantock@gmail.com>
2016-07-16 18:13:14 -0700

    initial commit of local work

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+var socket = io();
+
+// Update h1 tag
+j$('.room-title').text(room);
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
+
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = $('.messages');
+	var $message = $('<li class="list-group-item"></li>');
+	var messageBody = document.getElementById("messages");
+
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
+	messageBody.scrollTop = messageBody.scrollHeight;
+});
+
+// Handles submitting of new message
+var $form = $('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val('');
+});
